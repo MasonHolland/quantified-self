e795448c1f4821bcfcfd7a3bb6b9d36902d730b7
MasonHolland <masontholland@gmail.com>
2017-10-12 15:18:56 -0600

    changes for merge

---
+++ test/models/food-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const Food = require('../../lib/models/food')
+
+describe('', function() {
+  context('', function() {
+    it('', function() {
+
+    })
+  })
+})
